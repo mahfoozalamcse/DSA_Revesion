b2a45fcaf785570199fd05d7002d4efff60a2c27
mahfoozalamcse <amahfooz1007@gmail.com>
2025-11-02 23:28:26 +0530

    practice hashing part 2

---
+++ Hashing/HashingPart2.java
@@ -0,0 +1,21 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class HashingPart2 {
+    // Count distinct elements in an array
+    public static int countDistinctElements(int[] arr) {
+        // Your code here
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        return set.size();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int distinctCount = countDistinctElements(arr);
+        System.out.println("Number of distinct elements: " + distinctCount);
+    }
+}
