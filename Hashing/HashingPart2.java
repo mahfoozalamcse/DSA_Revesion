9a80aaae42f590a7af1756060e17fb70b0b3f0ed
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2026-04-08 21:47:35 +0530

    Update HashingPart2.java

--- Hashing/HashingPart2.java
+++ Hashing/HashingPart2.java
@@ -10,13 +10,12 @@ public class HashingPart2 {
         for (int num : arr) {
             set.add(num);
         }
-        return set.size();
-        
-        
+        return set.size();    
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 2, 3, 4, 4, 5};
         int distinctCount = countDistinctElements(arr);
         System.out.println("Number of distinct elements: " + distinctCount);
+        
     }
 }
